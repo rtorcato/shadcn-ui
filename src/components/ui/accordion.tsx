54846eee9b7d4bcf6abdfae7ca6272e3588882e6
Richard Torcato <rtorcato@me.com>
2024-08-07 12:09:12 -0400

    update

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -8,7 +8,9 @@ const Accordion = AccordionPrimitive.Root
 
 const AccordionItem = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Item>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item> & {
+    className?: string
+  }
 >(({ className, ...props }, ref) => (
   <AccordionPrimitive.Item
     ref={ref}
