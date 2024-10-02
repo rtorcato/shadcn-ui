873d7393635d9f06cb31aea18e554e2b9ef3fba4
Richard Torcato <rtorcato@me.com>
2024-10-02 18:25:00 -0400

    feat: update analyze bundle

--- build.mjs
+++ build.mjs
@@ -3,6 +3,9 @@ import path from 'path'
 import esbuild from 'esbuild'
 import { nodeExternalsPlugin } from 'esbuild-node-externals'
 
+// import { visualizer } from 'esbuild-visualizer'
+// import { analyze } from 'esbuild-plugin-analyze'
+
 async function getEntryPoints(dir) {
   const entries = await fs.readdir(dir, { withFileTypes: true })
   const entryPoints = entries
@@ -61,7 +64,7 @@ async function build() {
   ]
 
   console.log('All entry points:', allEntryPoints)
-  await esbuild.build({
+  const result = await esbuild.build({
     // entryPoints: ["src/index.ts"],
     entryPoints: allEntryPoints,
     outdir: 'dist',
@@ -72,9 +75,24 @@ async function build() {
     format: 'esm',
     target: ['esnext'],
     bundle: true,
+    metafile: true,
     splitting: true,
+    // logLevel: 'verbose',  // Add this line
     chunkNames: 'chunks/[name]-[hash]',
     external: ['react', 'react-dom'], // Add other external dependencies here
+    plugins: [
+      // visualizer({
+      //   filename: 'bundle-visualizer.html' || 'bundle.html', // Ensure the output file path is not undefined
+      //   // filename: 'dist/stats.html',
+      //   open: true, // Opens in browser automatically
+      //   format: 'json', // Other formats: 'html', 'json', 'treemap'
+      // }),
+      // analyze({ summaryOnly: true }), // Generates stats.json
+      nodeExternalsPlugin({
+        // These modules won't be externalized and will be included in the bundle
+        // allowList: ['some-module', 'another-module'],
+      }),
+    ],
     // plugins: [
     //   {
     //     name: 'make-all-packages-external',
@@ -84,8 +102,15 @@ async function build() {
     //     },
     //   },
     // ],
-    plugins: [nodeExternalsPlugin()],
   })
+  // console.log(result)
+  if (result.metafile) {
+    // Ensure that metafile exists before trying to write it
+    await fs.writeFile('meta.json', JSON.stringify(result.metafile))
+    console.log('Metafile generated: meta.json')
+  } else {
+    console.error('Metafile is undefined. Build might have failed.')
+  }
 }
 
 build().catch((e) => {
